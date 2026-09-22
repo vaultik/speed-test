388f3575219ecb43a0ffa6482eb7225cd3b5a0e9
George Vasilev <fondan.dolgoprundy@gmail.com>
2026-09-22 19:58:43 +0400

    Calculate average request speed

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
 import requests
 
+IMG_URL = 'https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fmoment%2F&ved=0CBYQjRxqFwoTCPieuqu1gpcDFQAAAAAdAAAAABA4&opi=89978449'
+
 
 def measure_single_request(url: str) -> tuple[float, int]:
     """
     Делаем один get запрос, дожидаемся полной загрузки ответа.
 
-    Возвращаем (время сек, объём байт)
+    Возвращаем (время сек, объём байт).
     """
     start = time.perf_counter()
 
@@ -21,7 +23,25 @@ def measure_single_request(url: str) -> tuple[float, int]:
     return elapsed, downloaded_bytes
 
 
-if __name__ == "__main__":
-    elapsed, downloaded_bytes = measure_single_request('https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fmoment%2F&ved=0CBYQjRxqFwoTCPieuqu1gpcDFQAAAAAdAAAAABA4&opi=89978449')
-    print(f'Time: {elapsed:.3f}s, Bytes: {downloaded_bytes}')
+def measure_requests(url: str, count: int = 10) -> list[tuple[float, int]]:
+    """Делаем несколько последовательных запросов."""
+    results = []
 
+    for _ in range(count):
+        result = measure_single_request(url)
+        results.append(result)
+
+    return results
+
+
+if __name__ == "__main__":
+    results = measure_requests(IMG_URL)
+
+    times = [result[0] for result in results]
+    sizes = [result[1] for result in results]
+
+    average_time = sum(times) / len(times)
+    average_size = sum(sizes) / len(sizes)
+    speed = average_size / average_time / (1024 * 1024)
+
+    print(f'Average time: {average_time:.3f}s, Average size {average_size}, Speed {speed}МБ/с')
